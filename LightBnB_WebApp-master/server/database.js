74386cd555827ced321a351f2b1a66a756990f64
Hsing-I Wang <hsing.sophie@gmail.com>
2021-02-05 21:50:24 +0000

    add properties

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -150,17 +150,18 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const values = [property.owner_id, property.title, property.description, property.thumbnail_photo_url,
-  property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province,
-  property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms,
-  property.number_of_bedrooms];
-  const query = `INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url,
-    cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms,
-    number_of_bedrooms) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`;
-  return pool.query(query, values)
+  return pool.query(`
+    INSERT INTO properties(owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, 
+    parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;`,
+    [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url,
+    property.cost_per_night, property.parking_spaces, property.number_of_bathrooms,
+    property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code])
     .then(data => {
-      return data.rows;
+      console.log(data.rows[0]);
+      return data;
     }).catch(() => {
+      console.log("error");
       return null;
     });
 };
